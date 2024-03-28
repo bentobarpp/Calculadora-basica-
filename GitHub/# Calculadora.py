b7f34ac3ca258aeeb85f2d39ed511d4049e61e11
bentobarpp <bentobarpp@gmail.com>
2024-03-28 12:21:41 -0300

    calculadora

---
+++ GitHub/# Calculadora.py
@@ -0,0 +1,24 @@
+# Calculadora 
+print("Digite um numero: ")
+numero1= int(input())
+
+print("Digite um operador: ")
+operador = input()
+
+print("Digite um segundo numero: ")
+numero2 = int((input)())
+
+if operador == '+':
+    resultado = numero1 + numero2
+    print(resultado)
+elif operador =='-':
+    resultado = numero1 - numero2
+    print(resultado)
+elif operador == '*':
+    resultado = numero1 * numero2
+    print(resultado)
+elif operador == '/':
+    resultado = numero1 / numero2
+    print(resultado)
+else:
+    print("Operador incorreto! ")
